50a4ac1924fa5044a7fee2c8743b0012d8610413
amie-dev <learn.amie01@gmail.com>
2026-04-04 09:42:08 +0530

    cleanup

--- App.tsx
+++ App.tsx
@@ -5,20 +5,20 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+
+import { StatusBar, StyleSheet,  View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  
 
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      
     </SafeAreaProvider>
   );
 }
@@ -28,10 +28,7 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      
     </View>
   );
 }
